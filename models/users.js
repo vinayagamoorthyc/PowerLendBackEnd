5ae0a7933914df4dab862efb9a9e2433704da1a5
vinayagamoorthyc <sanjaiiti2709@gmail.com>
2024-01-26 10:42:46 +0530

    0.0.0.1

---
+++ models/users.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+    username: String,
+    email: String,
+    phone: Number,
+    password: String,
+    role: {
+        type: String,
+        default: "user"
+    }
+});
+
+const userModel = mongoose.model("users", UserSchema);
+module.exports = userModel;
--- server.js
+++ server.js
@@ -5,12 +5,44 @@ const UserexpModel = require("./models/userexp.js");
 const UserreportModel = require("./models/userreport.js");
 const ProductModel = require("./models/products.js");
 const CartModel = require("./models/cart.js");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const cookieParser = require('cookie-parser');
+const UserModel = require("./models/users.js");
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:3000"],
+  methods: ["GET", "POST"],
+  credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 mongoose.connect("mongodb+srv://VinayagaMoorthy27:Vinayaga2709@vinayagadb.o991msv.mongodb.net/Mern_Project?retryWrites=true&w=majority");
+
+const verifyUser=(req, res, next)=>{
+  const token = req.cookies.token;
+  if(!token){
+    return res.json("Token missing");
+  }else{
+    jwt.verify(token, "jwt-secret-key", (err, decoded)=>{
+      if(err){
+        return res.json("error with token");
+      }else{
+        if(decoded.role === "admin"){
+          next();
+        }else{
+          return res.json("not an admin");
+        }
+      }
+    })
+  }
+}
+
+app.get('/admin', verifyUser, (req, res)=>{
+  res.json("success");
+})
 
 // -----------------------------------------Experience--------------------------------------
 
@@ -101,6 +133,38 @@ app.delete("/deleteCart/:id", (req, res)=>{
   CartModel.findByIdAndDelete({_id: id})
   .then(e=>res.json(e))
   .catch(err=>res.json(err))
+})
+
+// -----------------------------------------Cart details--------------------------------------
+
+app.post("/register",(req, res)=>{
+  const {username, email, phone, password}=req.body;
+  bcrypt.hash(password, 10)
+  .then(hash=>{
+    UserModel.create({username, email, phone, password: hash})
+    .then(e=>res.json("Success!"))
+    .catch(err=>res.json(err))
+  }).catch(err=>res.json(err))
+});
+app.post("/login", (req, res)=>{
+  const {email, password}=req.body;
+  UserModel.findOne({email: email})
+  .then(user =>{
+    if(user){
+      bcrypt.compare(password, user.password, (err, response)=>{
+        if(response){
+          const token = jwt.sign({email: user.email, role: user.role},
+            "jwt-secret-key", {expiresIn: "1d"});
+            res.cookie('token', token);
+            return res.json({Status: "success", role: user.role});
+        }else{
+          return res.json("incorrect password!");
+        }
+      })
+    }else{
+      return res.json("no record exist");
+    }
+  })
 })
 
 app.listen(3002, ()=>{
