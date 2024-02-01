e188d392b22ff8f7270b1c3d9c4ea388a7b797b7
vinayagamoorthyc <sanjaiiti2709@gmail.com>
2024-02-01 13:40:25 +0530

    0.0.0.1

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const UserModel = require("./models/users.js");
 
 const app = express();
 app.use(cors({
-  origin: ["http://localhost:3000"],
+  origin: ["https://powertoolsrental.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
